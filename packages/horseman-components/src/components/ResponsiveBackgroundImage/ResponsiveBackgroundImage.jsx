d1dd5a00d2f9d5e17653f2e9a9c8a78ef33da56f
Jared Meyering <jared.meyering@gmail.com>
2017-10-30 14:13:37 +0000

    prettier

--- packages/horseman-components/src/components/ResponsiveBackgroundImage/ResponsiveBackgroundImage.jsx
+++ packages/horseman-components/src/components/ResponsiveBackgroundImage/ResponsiveBackgroundImage.jsx
@@ -2,9 +2,7 @@ import BackgroundImage from "../BackgroundImage";
 import dimensions from "../../providers/Dimensions";
 
 // Check for Window before importing dimension so we don't break SSR
-const ResponsiveBackgroundImage = typeof window !== "undefined" ?
-  dimensions(BackgroundImage) :
-  BackgroundImage;
+const ResponsiveBackgroundImage =
+  typeof window !== "undefined" ? dimensions(BackgroundImage) : BackgroundImage;
 
 export default ResponsiveBackgroundImage;
-

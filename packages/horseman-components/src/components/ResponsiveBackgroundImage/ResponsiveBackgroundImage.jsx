7d11c00b4ba90f54bc935a07dc55c1dc54d5f02b
Jared Meyering <jared.meyering@gmail.com>
2017-12-04 15:35:23 +0000

    attempt to fix the responsivebackgroundimage problem

--- packages/horseman-components/src/components/ResponsiveBackgroundImage/ResponsiveBackgroundImage.jsx
+++ packages/horseman-components/src/components/ResponsiveBackgroundImage/ResponsiveBackgroundImage.jsx
@@ -1,8 +1,4 @@
 import BackgroundImage from "../BackgroundImage";
 import dimensions from "../../providers/dimensions";
 
-// Check for Window before importing dimension so we don't break SSR
-const ResponsiveBackgroundImage =
-  typeof window !== "undefined" ? dimensions(BackgroundImage) : BackgroundImage;
-
-export default ResponsiveBackgroundImage;
+export default dimensions(BackgroundImage);

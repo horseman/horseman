bf47a64e9fc7edfa541e4c66769f684863c3bdbc
Jared Meyering <jared.meyering@gmail.com>
2018-01-08 13:37:39 -0600

    add horseman-core into the fold of the monorepo

---
+++ packages/horseman-core/src/__tests__/index-test.js
@@ -0,0 +1,46 @@
+import * as horseman from "../";
+
+import paginator from "../providers/paginator";
+import prefetch from "../providers/prefetch";
+import { ConnectedResourceProvider } from "../providers/ResourceProvider";
+import { ConnectedPaginationProvider } from "../providers/PaginationProvider";
+import horsemanReducer from "../reducers/horsemanReducer";
+import horsemanRouteReducer from "../reducers/horsemanRouteReducer";
+import ActionFactory from "../ActionFactory";
+
+describe("horseman.js index", () => {
+  describe("should export", () => {
+    test("reducers", () => {
+      expect(typeof horseman.horsemanReducers.horsemanResources).toBe(
+        "function",
+      );
+
+      expect(horseman.horsemanReducers.horsemanResources).toEqual(
+        horsemanReducer,
+      );
+
+      expect(typeof horseman.horsemanReducers.horsemanRoutes).toBe("function");
+      expect(horseman.horsemanReducers.horsemanRoutes).toEqual(
+        horsemanRouteReducer,
+      );
+    });
+    test("the PaginationProvider", () => {
+      expect(horseman.PaginationProvider).toEqual(ConnectedPaginationProvider);
+    });
+    test("the ActionFactory", () => {
+      expect(horseman.ActionFactory).toEqual(ActionFactory);
+    });
+    test("the ResourceProvider", () => {
+      expect(horseman.ResourceProvider).toEqual(ConnectedResourceProvider);
+    });
+    test("the prefetch provider", () => {
+      expect(horseman.prefetch).toEqual(prefetch);
+    });
+    test("the paginator provider", () => {
+      expect(horseman.paginator).toEqual(paginator);
+    });
+    test("the Correct Number of items", () => {
+      expect(Object.keys(horseman)).toHaveLength(6);
+    });
+  });
+});

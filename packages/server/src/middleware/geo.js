73659fae7b488ca2257175fb641af876f7ecf417
Marc Tanis <marcato15@gmail.com>
2018-09-11 16:50:33 -0500

    Fix typo

--- packages/server/src/middleware/geo.js
+++ packages/server/src/middleware/geo.js
@@ -1,13 +1,15 @@
 const iplocation = require("iplocation");
 
 const geoMiddleware = (req, res, next) => {
-  iplocation(req.ip).then(result => {
-    req.geoip = result;
-    next();
-  }).catch( () => {
-    req.geoip = {attributes:null};
-    next();
-  });
+  iplocation(req.ip)
+    .then(result => {
+      req.geoip = { attributes: result };
+      next();
+    })
+    .catch(() => {
+      req.geoip = { attributes: null };
+      next();
+    });
 };
 
 export default geoMiddleware;

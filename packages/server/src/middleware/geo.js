2424ea7ad65ec931f073976537502c5b7269efdc
Marc Tanis <marcato15@gmail.com>
2018-09-12 10:14:32 -0500

    ignore health checks

--- packages/server/src/middleware/geo.js
+++ packages/server/src/middleware/geo.js
@@ -1,6 +1,11 @@
 const iplocation = require("iplocation");
 
 const geoMiddleware = (req, res, next) => {
+  if (parseInt(ip.split(".")[0], 10) === 10) {
+    req.geoip = { attributes: null };
+    next();
+    return;
+  }
   iplocation(req.ip)
     .then(result => {
       req.geoip = { attributes: result };
